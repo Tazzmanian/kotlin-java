e8212e68db2cb29c71e3fa7d14b75ebfd61654b8
Todorov, Teodor (EXTERN: MS) <teodor.todorov@car-digital.eu>
2019-05-17 11:02:40 +0300

    aliases

---
+++ test/src/main/kotlin/org/test/Alias.kt
@@ -0,0 +1,23 @@
+package org.test.alias
+
+class A {
+    inner class Inner
+}
+class B {
+    inner class Inner
+}
+
+typealias AInner = A.Inner
+typealias BInner = B.Inner
+
+typealias Predicate<T> = (T) -> Boolean
+
+fun foo(p: Predicate<Int>) = p(42)
+
+fun main() {
+    val f: (Int) -> Boolean = { it > 0 }
+    println(foo(f)) // prints "true"
+
+    val p: Predicate<Int> = { it > 0 }
+    println(listOf(1, -2).filter(p)) // prints "[1]"
+}
